60bfa8ab824d6dc57184d6b1fbadc83090c86b11
Rakibul hasan <mdrakibulhasan12346@gmail.com>
2025-11-10 19:44:38 +0600

    change blog card bg color

--- src/components/blog/BlogCard.jsx
+++ src/components/blog/BlogCard.jsx
@@ -25,7 +25,7 @@ export default function BlogCard({ initialData, searchQuery }) {
       {filteredData.map((blog) => (
         <div
           key={blog.id}
-          className="flex flex-col gap-3 rounded-xl bg-background-light dark:bg-card-dark shadow-md border border-[#f6efcb] dark:border-border-dark overflow-hidden group hover:shadow-xl transition-all duration-300 hover:-translate-y-1"
+          className="flex flex-col gap-3 rounded-xl bg-white/50 dark:bg-card-dark shadow-md border border-[#f6efcb] dark:border-border-dark overflow-hidden group hover:shadow-xl transition-all duration-300 hover:-translate-y-1"
         >
           <div className="relative w-full aspect-5/3 bg-gray-200 dark:bg-gray-700 overflow-hidden">
             <Image
