ce7e203b36c27dbfd38ea6d32059870e49f79f2b
Mehedi <mehedi25122003@gmail.com>
2025-11-25 18:36:40 +0600

    feat: Update BuyDetailsPage to fetch property data from API and enhance property structure

--- next.config.mjs
+++ next.config.mjs
@@ -29,6 +29,11 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'placehold.co',
       },
+      {
+        protocol: 'https',
+        hostname: 'quiahgroup1backend.mtscorporate.com',
+        pathname: '/uploads/**',
+      },
     ],
     formats: ['image/avif', 'image/webp'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
