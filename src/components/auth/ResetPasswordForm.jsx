39d2c801a17a1e1da43539e8ef87dc40f1a25e4e
shanjid5566 <shanjidahmed66@gmail.com>
2025-12-25 12:22:44 +0600

    register password field error solved

--- src/components/auth/ResetPasswordForm.jsx
+++ src/components/auth/ResetPasswordForm.jsx
@@ -42,8 +42,28 @@ const ResetPasswordForm = () => {
             return;
         }
 
-        if (password.length < 6) {
-            setError('Password must be at least 6 characters');
+        if (password.length < 8) {
+            setError('Password must be at least 8 characters');
+            return;
+        }
+
+        if (!/[A-Z]/.test(password)) {
+            setError('Password must contain at least one uppercase letter');
+            return;
+        }
+
+        if (!/[a-z]/.test(password)) {
+            setError('Password must contain at least one lowercase letter');
+            return;
+        }
+
+        if (!/[0-9]/.test(password)) {
+            setError('Password must contain at least one number');
+            return;
+        }
+
+        if (!/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(password)) {
+            setError('Password must contain at least one special character');
             return;
         }
 
