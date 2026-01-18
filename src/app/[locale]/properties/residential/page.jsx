79f050fc50c423b86ede6b45c6eb5b9b868ca205
Mehedi <mehedi25122003@gmail.com>
2026-01-18 15:33:30 +0600

    new development section api changed

--- src/app/[locale]/properties/residential/page.jsx
+++ src/app/[locale]/properties/residential/page.jsx
@@ -53,7 +53,7 @@ export default function ResidentialPage() {
       setLoadingDevelopments(true);
       setDevError(null);
       try {
-        const res = await api.get('/properties?propertyType=NEW_DEVELOPMENT', { signal: controller.signal });
+        const res = await api.get('/properties?listingType=DEVELOPMENT', { signal: controller.signal });
         // api.get returns response.data from axios; backend may nest under `data`
         const data = res?.data || res;
         let props = data?.properties || data?.data?.properties || data?.data || data?.items || [];
